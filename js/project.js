aa3cd17aecb401f3012a9b14a4c56cd00c7cd7bd
Lunasphys <91267490+Lunasphys@users.noreply.github.com>
2022-08-17 18:19:13 +0200

    projet ok

---
+++ js/project.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
+
+function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+}
+
+function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+}
+
+function myFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+}
